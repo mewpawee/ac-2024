04c79f7e222d6167fa5c8d0940e45e9f18fe2b34
Pawee Tantivasdakarn <pawee@alphainnovationslab.io>
2024-12-10 21:41:45 +0700

    add 2-2 solution

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -16,7 +16,22 @@ fn main() {
 
     let mut safe_count = 0;
     for line in lines.clone() {
-        let safe = is_safe(line);
+        // let mut safe = is_safe(line.clone());
+
+        // // try to remove index and check again if it not safe
+        // if !safe {
+        //     safe = fixer(line.clone());
+        //     // for i in 0..line.len() {
+        //     //     let mut line_copy = line.clone();
+        //     //     line_copy.remove(i);
+        //     //     safe = is_safe(line_copy);
+        //     //     if safe {
+        //     //         break;
+        //     //     }
+        //     // }
+        // }
+        let safe = run_with_fixer(line.clone());
+
         if safe {
             safe_count += 1
         }
@@ -25,6 +40,28 @@ fn main() {
     // first_elements.sort();
     // second_elements.sort();
     println!("{:?}", safe_count)
+}
+
+fn run_with_fixer(data: Vec<i32>) -> bool {
+    let mut safe = is_safe(data.clone());
+
+    // try to remove index and check again if it not safe
+    if !safe {
+        safe = fixer(data.clone());
+    }
+    safe
+}
+
+fn fixer(data: Vec<i32>) -> bool {
+    for i in 0..data.len() {
+        let mut line_copy = data.clone();
+        line_copy.remove(i);
+        let safe = is_safe(line_copy);
+        if safe {
+            return true;
+        }
+    }
+    false
 }
 
 fn is_safe(data: Vec<i32>) -> bool {
@@ -36,6 +73,10 @@ fn is_safe(data: Vec<i32>) -> bool {
 
     for i in 1..data.len() {
         let diff = data[i] - data[i - 1];
+
+        if diff == 0 {
+            return false;
+        }
 
         // diff greater than 3 then unsafe, return false
         if diff.abs() > 3 {
@@ -61,6 +102,7 @@ fn is_safe(data: Vec<i32>) -> bool {
 mod tests {
     use super::*;
 
+    // test first
     #[test]
     fn one() {
         let result = is_safe(vec![7, 6, 4, 2, 1]);
@@ -89,6 +131,40 @@ mod tests {
     #[test]
     fn six() {
         let result = is_safe(vec![1, 3, 6, 7, 9]);
+        assert!(result);
+    }
+
+    // test second
+    #[test]
+    fn first_b() {
+        let result = run_with_fixer(vec![7, 6, 4, 2, 1]);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn sec_b() {
+        let result = run_with_fixer(vec![1, 2, 7, 8, 9]);
+        assert!(!result);
+    }
+    #[test]
+    fn trd_b() {
+        let result = run_with_fixer(vec![9, 7, 6, 2, 1]);
+        assert!(!result);
+    }
+    #[test]
+    fn frt_b() {
+        let result = run_with_fixer(vec![1, 3, 2, 4, 5]);
+        assert!(result);
+    }
+    #[test]
+    fn fifth_b() {
+        let result = run_with_fixer(vec![8, 6, 4, 4, 1]);
+        assert!(result);
+    }
+    #[test]
+    fn six_b() {
+        let result = run_with_fixer(vec![1, 3, 6, 7, 9]);
         assert!(result);
     }
 }
