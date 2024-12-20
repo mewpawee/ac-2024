42f426dd2836cfe561ba77c75ced7a6e5f7fcf75
mewpawee <pawee@alphainnovationslab.io>
2024-12-20 09:43:13 +0700

    add rules

--- src/bin/5.rs
+++ src/bin/5.rs
@@ -1,21 +1,32 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 
 const INPUT: &str = include_str!("../../inputs/5.in");
 
 fn main() {
     // parse rules_raw and updates_raw
     let (rules_raw, updates_raw) = INPUT.split("\n\n").take(2).collect_tuple().unwrap();
-    // parse rules
-    let rules: Vec<_> = rules_raw
-        .lines()
-        .map(|line| {
-            line.split("|")
-                .map(|y| y.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>()
-        })
-        .collect();
-    println!("{:?}", rules[0][1]);
-    let updates: Vec<_> = updates_raw
+    let mut rules_forward = HashMap::new();
+    let mut rules_backward = HashMap::new();
+    for line in rules_raw.lines() {
+        if let Ok(values) = line
+            .split('|')
+            .map(str::trim)
+            .map(str::parse::<i32>)
+            .collect::<Result<Vec<i32>, _>>()
+        {
+            if values.len() >= 2 {
+                rules_forward.insert(values[0], values[1]);
+                rules_backward.insert(values[1], values[0]);
+            }
+        }
+    }
+
+    // for (key, value) in &rules {
+    //     println!("{}: {}", key, value);
+    // }
+
+    let updates: Vec<Vec<i32>> = updates_raw
         .lines()
         .map(|line| {
             line.split(",")
@@ -23,55 +34,21 @@ fn main() {
                 .collect::<Vec<i32>>()
         })
         .collect();
-    println!("{:?}", updates[0]);
-    // let mut texts: Vec<&str> = vec![];
-    // // capture mul(number,number), do() and don't
-    // let re = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)").unwrap();
-    // for cap in re.find_iter(INPUT) {
-    //     let text = cap.as_str();
-    //     texts.push(text);
-    // }
-
-    // let mut is_ok = true;
-    // let mut sum = 0;
-
-    // for text in texts {
-    //     // let text = "mul(125,904)";
-    //     match parse_text(text) {
-    //         Cmds::Mul(a, b) => {
-    //             if is_ok {
-    //                 sum += a * b
-    //             }
-    //         }
-    //         Cmds::Do => is_ok = true,
-    //         Cmds::Dont => is_ok = false,
-    //         Cmds::Invalid => println!("Something went wrong, this case shouldn't happen"),
-    //     }
-    // }
-    // println!("sum: {:?}", sum);
+    check_rules(updates[0].clone(), rules_forward, rules_backward);
 }
 
-enum Cmds {
-    Mul(i32, i32),
-    Do,
-    Dont,
-    Invalid, // Add a variant to handle invalid input
-}
-
-fn parse_text(input: &str) -> Cmds {
-    if let Some(stripped) = input.strip_prefix("mul(") {
-        if let Some(stripped) = stripped.strip_suffix(')') {
-            let parts: Vec<&str> = stripped.split(',').collect();
-            if parts.len() == 2 {
-                if let (Ok(num1), Ok(num2)) = (parts[0].parse::<i32>(), parts[1].parse::<i32>()) {
-                    return Cmds::Mul(num1, num2);
-                }
-            }
-        }
-    } else if input == "do()" {
-        return Cmds::Do;
-    } else if input == "don't()" {
-        return Cmds::Dont;
+fn check_rules(
+    update: Vec<i32>,
+    rules_forward: HashMap<i32, i32>,
+    rules_backward: HashMap<i32, i32>,
+) -> bool {
+    for number in update {
+        // check before
+        let before = rules_backward.get(&number).unwrap();
+        println!("{:?}", before);
+        // check after
+        let after = rules_forward.get(&number).unwrap();
+        println!("{:?}", after);
     }
-    Cmds::Invalid // Return Invalid for any non-matching input
+    true
 }
